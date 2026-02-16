cd0fa7dd75499e599168c25435c8eec8c6196c4f
hbb <kok.bing@qq.com>
2026-02-16 07:25:41 +0000

    [prism_all] fix: hard depend on qt 6.8.3 (1.0.7)
    | Submodule prism b15e721..0776aba:
    |   > ci: test vcpkg publish
    | Submodule prism_container 981de5a..4d92bb6:
    |   > ci: test vcpkg publish
    | Submodule prism_qt_core 80da53a..22ef5c4:
    |   > ci: test vcpkg publish
    | Submodule prism_qt_modular 1b0b33d..3d6aae1:
    |   > ci: test vcpkg publish
    | Submodule prism_qt_ui ea280c6..24a9329:
    |   > fix: hard depend on qt 6.8.3
    |   > ci: test vcpkg publish

---
+++ ports/prism-container/portfile.cmake
@@ -0,0 +1,34 @@
+file(READ "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cus_info.json" _contents)
+string(JSON GIT_HEAD GET "${_contents}" git-head)
+
+vcpkg_from_git(
+  OUT_SOURCE_PATH SOURCE_PATH
+  URL https://github.com/cpp-prism/prism_container.git
+  REF ${GIT_HEAD}
+  HEAD_REF master
+)
+
+
+#https://learn.microsoft.com/en-us/vcpkg/examples/packaging-github-repos
+vcpkg_cmake_configure( SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_install()
+
+
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
+
+file(
+    INSTALL "${SOURCE_PATH}/usage"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+
+
+
